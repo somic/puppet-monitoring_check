eca4e449e0430286b52cebe8742576c555d0002f
Josh Snyder <josnyder@yelp.com>
2014-08-04 14:39:35 -0700

    Clean up the mess

--- spec/defines/monitoring_check_spec.rb
+++ spec/defines/monitoring_check_spec.rb
@@ -15,20 +15,47 @@ describe 'monitoring_check' do
         .with_handlers('default') \
         .with_interval(default_interval) \
         .with_command('bar') \
-        .with_custom({"runbook"=>"http://gronk", "dependencies"=>[], "ticket"=>false, "irc_channels"=>:undef, "tip"=>false, "project"=>false, "alert_after"=>"0", "page"=>true, "realert_every"=>"1", "sla"=>"No SLA defined.", "team"=>"operations", "notification_email"=>"undef"})
-        #.with_custom({ "dependencies"=>[], "ticket"=>false, "project"=>false, "runbook"=>"http://gronk", "irc_channels"=>:undef, "realert_every"=>"1", "sla"=>"No SLA defined", "alert_after"=>"0", "tip"=>false, "page"=>true, "team"=>"operations", "notification_email"=>:undef })
+        .with_custom({"runbook"=>"http://gronk",
+          "dependencies"=>[],
+          "ticket"=>false,
+          "irc_channels"=>:undef,
+          "tip"=>false,
+          "project"=>false,
+          "alert_after"=>"0",
+          "page"=>true,
+          "realert_every"=>"1",
+          "sla"=>"No SLA defined.",
+          "team"=>"operations",
+          "notification_email"=>"undef"
+        })
     end
   end
+
   context "bad runbook (not a uri)" do
     let(:params) { {:command => 'bar', :runbook => 'gronk'} }
     it do
       expect { should contain_monitoring_check('examplecheck') }.to raise_error(Exception, /does not match/)
     end
   end
+
   context "custom team" do
     let(:params) { {:command => 'bar', :runbook => 'http://gronk', :team => 'other'} }
-    it { should contain_sensu__check('examplecheck').with_custom({"runbook"=>"http://gronk", "dependencies"=>[], "ticket"=>false, "irc_channels"=>:undef, "tip"=>false, "project"=>false, "alert_after"=>"0", "page"=>false, "realert_every"=>"1", "sla"=>"No SLA defined.", "team"=>"other", "notification_email"=>"undef"}) }
+    it { should contain_sensu__check('examplecheck').with_custom({
+      "runbook"=>"http://gronk",
+      "dependencies"=>[],
+      "ticket"=>false,
+      "irc_channels"=>:undef,
+      "tip"=>false,
+      "project"=>false,
+      "alert_after"=>"0",
+      "page"=>false,
+      "realert_every"=>"1",
+      "sla"=>"No SLA defined.",
+      "team"=>"other",
+      "notification_email"=>"undef"})
+    }
   end
+
   context "custom team (nonexistent)" do
     let(:params) { {:command => 'bar', :runbook => 'http://gronk', :team => 'doesnotexist'} }
     it do
@@ -37,37 +64,114 @@ describe 'monitoring_check' do
   end
   context "with one dependency" do
     let(:params) { {:command => 'bar', :runbook => 'http://gronk', :dependencies => 'dep1'} }
-    it { should contain_sensu__check('examplecheck').with_custom({"runbook"=>"http://gronk", "dependencies"=>["dep1"], "ticket"=>false, "irc_channels"=>:undef, "tip"=>false, "project"=>false, "alert_after"=>"0", "page"=>false, "realert_every"=>"1", "sla"=>"No SLA defined.", "team"=>"operations", "notification_email"=>"undef"}) }
+    it { should contain_sensu__check('examplecheck').with_custom({"runbook"=>"http://gronk",
+      "dependencies"=>["dep1"],
+      "ticket"=>false,
+      "irc_channels"=>:undef,
+      "tip"=>false,
+      "project"=>false,
+      "alert_after"=>"0",
+      "page"=>false,
+      "realert_every"=>"1",
+      "sla"=>"No SLA defined.",
+      "team"=>"operations",
+      "notification_email"=>"undef"})
+    }
   end
+
   context "with two dependency" do
     let(:params) { {:command => 'bar', :runbook => 'http://gronk', :dependencies => ['dep1', 'dep2']} }
-    it { should contain_sensu__check('examplecheck').with_custom({"runbook"=>"http://gronk", "dependencies"=>["dep1", "dep2"], "ticket"=>false, "irc_channels"=>:undef, "tip"=>false, "project"=>false, "alert_after"=>"0", "page"=>false, "realert_every"=>"1", "sla"=>"No SLA defined.", "team"=>"operations", "notification_email"=>"undef"}) }
+    it { should contain_sensu__check('examplecheck').with_custom({
+      "runbook"=>"http://gronk",
+      "dependencies"=> ["dep1","dep2"],
+      "ticket"=>false,
+      "irc_channels"=>:undef,
+      "tip"=>false,
+      "project"=>false,
+      "alert_after"=>"0",
+      "page"=>false,
+      "realert_every"=>"1",
+      "sla"=>"No SLA defined.",
+      "team"=>"operations",
+      "notification_email"=>"undef"
+    })}
   end
 
   context "with custom" do
     let(:params) { {:command => 'bar', :runbook => 'http://gronk', :sensu_custom => { 'foo' => 'bar' } } }
-    it { should contain_sensu__check('examplecheck').with_custom(
-{"runbook"=>"http://gronk", "dependencies"=>[], "ticket"=>false, "irc_channels"=>:undef, "tip"=>false, "project"=>false, "alert_after"=>"0", "foo"=>"bar", "page"=>false, "realert_every"=>"1", "sla"=>"No SLA defined.", "team"=>"operations", "notification_email"=>"undef"}
-     ) }
-  end
-  context "with custom - overriding" do
-    let(:params) { {:command => 'bar', :runbook => 'http://gronk', :sensu_custom => { 'team' => 'overridden' } } }
-    it { should contain_sensu__check('examplecheck').with_custom(
-{"runbook"=>"http://gronk", "dependencies"=>[], "ticket"=>false, "irc_channels"=>:undef, "tip"=>false, "project"=>false, "alert_after"=>"0", "page"=>false, "realert_every"=>"1", "sla"=>"No SLA defined.", "team"=>"overridden", "notification_email"=>"undef"}
+    it { should contain_sensu__check('examplecheck').with_custom({
+        "runbook"=>"http://gronk",
+        "dependencies"=>[],
+        "ticket"=>false,
+        "irc_channels"=>:undef,
+        "tip"=>false,
+        "project"=>false,
+        "alert_after"=>"0",
+        "foo"=>"bar",
+        "page"=>false,
+        "realert_every"=>"1",
+        "sla"=>"No SLA defined.",
+        "team"=>"operations",
+        "notification_email"=>"undef"
+      }
     )}
   end
+
+  context "with custom - overriding" do
+    let(:params) { {:command => 'bar', :runbook => 'http://gronk', :sensu_custom => { 'team' => 'overridden' } } }
+    it { should contain_sensu__check('examplecheck').with_custom({
+        "runbook"=>"http://gronk",
+        "dependencies"=>[],
+        "ticket"=>false,
+        "irc_channels"=>:undef,
+        "tip"=>false,
+        "project"=>false,
+        "alert_after"=>"0",
+        "page"=>false,
+        "realert_every"=>"1",
+        "sla"=>"No SLA defined.",
+        "team"=>"overridden",
+        "notification_email"=>"undef"
+      }
+    )}
+  end
+
   context "nowake, nopage" do
     let(:params) { {:command => 'bar', :runbook => 'http://gronk', :page => 'false'} }
-    it { should contain_sensu__check('examplecheck').with_custom(
-{"runbook"=>"http://gronk", "dependencies"=>[], "ticket"=>false, "irc_channels"=>:undef, "tip"=>false, "project"=>false, "alert_after"=>"0", "page"=>false, "realert_every"=>"1", "sla"=>"No SLA defined.", "team"=>"operations", "notification_email"=>"undef"}
-    ) }
+    it { should contain_sensu__check('examplecheck').with_custom({
+        "runbook"=>"http://gronk",
+        "dependencies"=>[],
+        "ticket"=>false,
+        "irc_channels"=>:undef,
+        "tip"=>false,
+        "project"=>false,
+        "alert_after"=>"0",
+        "page"=>false,
+        "realert_every"=>"1",
+        "sla"=>"No SLA defined.",
+        "team"=>"operations",
+        "notification_email"=>"undef"
+      }
+    )}
   end
 
   context "With a SLA defined" do
     let(:params) { {:command => 'bar', :runbook => 'http://gronk', :page => 'false', :sla=>'custom SLA'} }
-    it { should contain_sensu__check('examplecheck').with_custom(
-{"runbook"=>"http://gronk", "dependencies"=>[], "ticket"=>false, "irc_channels"=>:undef, "tip"=>false, "project"=>false, "alert_after"=>"0", "page"=>false, "realert_every"=>"1", "sla"=>"custom SLA", "team"=>"operations", "notification_email"=>"undef"}
-    ) }
+    it { should contain_sensu__check('examplecheck').with_custom({
+        "runbook"=>"http://gronk",
+        "dependencies"=>[],
+        "ticket"=>false,
+        "irc_channels"=>:undef,
+        "tip"=>false,
+        "project"=>false,
+        "alert_after"=>"0",
+        "page"=>false,
+        "realert_every"=>"1",
+        "sla"=>"custom SLA",
+        "team"=>"operations",
+        "notification_email"=>"undef"
+      }
+    )}
   end
 
   context "sudo" do
