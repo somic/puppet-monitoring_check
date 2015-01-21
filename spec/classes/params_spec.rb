4a128402ef143757dd15da3277df7b67330c0780
Kyle Anderson <kwa@yelp.com>
2015-01-21 11:42:26 -0800

    Added send-test-sensu-alert script -a

--- spec/classes/params_spec.rb
+++ spec/classes/params_spec.rb
@@ -3,14 +3,21 @@ require 'spec_helper'
 describe 'monitoring_check::params' do
 
   context "by default" do
-    let(:facts) { { :lsbdistid => 'Ubuntu', :osfamily => 'Debian', :lsbdistcodename => 'lucid', :operatingsystem => 'Ubuntu', :ipaddress => '127.0.0.1', :puppetversion => '3.6.2' } }
     it { should compile }
     it { should_not contain_file('/etc/facter/facts.d/override_sensu_checks_to.txt') }
+    it { should contain_file('/usr/bin/send-test-sensu-alert') }
+    it { should have_file_resource_count(2)}
   end
   
   context "When the override_sensu_checks_to fact is present" do
-    let(:facts) { { :lsbdistid => 'Ubuntu', :osfamily => 'Debian', :lsbdistcodename => 'lucid', :operatingsystem => 'Ubuntu', :ipaddress => '127.0.0.1', :puppetversion => '3.6.2', :override_sensu_checks_to => 'test_user' } }
+    let(:facts) { { :override_sensu_checks_to => 'test_user' } }
     it { should contain_file('/etc/facter/facts.d/override_sensu_checks_to.txt') }
+  end
+
+  context "When given a differnet path for binaries" do
+    let(:params) {{ :bin_path => '/special_bin' }}
+    it { should contain_file('/special_bin/send-test-sensu-alert') }
+    it { should have_file_resource_count(2)}
   end
 
 end
