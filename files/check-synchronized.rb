4e49589e1caba2feb88f973ff7432c30ecb4d9c7
Dmitriy Samovskiy <dmits@yelp.com>
2015-04-17 14:02:27 -0700

    working version of check-synchronized.rb

--- files/check-synchronized.rb
+++ files/check-synchronized.rb
@@ -1,6 +1,6 @@
 #!/opt/sensu/embedded/bin/ruby
 
-$: << "#{File.dirname(__FILE__)}/lib"
+$: << File.dirname(__FILE__) + '/lib'
 
 require 'rubygems'
 require 'sensu-plugin/check/cli'
@@ -22,46 +22,48 @@ class CheckSynchronized < Sensu::Plugin::Check::CLI
     :description => 'path to synchronized check config json file',
     :required => true
 
-  attr_accessor :check, :check_name
+  attr_accessor :check, :check_name, :new_check
   attr_reader :configuration
 
   def run
-    File.open(config[:config]) { |f| @configuration = JSON.parse(f.read) }
-    File.open("#{configuration['sensu_checks_dir']}/#{config[:check]}.json") { |f|
-      check_json = JSON.parse(f.read)["checks"]
+    File.open(config[:config]) { |f|
+      @configuration = JSON.parse(f.read, :symbolize_names => true) }
+    File.open("#{configuration[:sensu_checks_dir]}/#{config[:check]}.json") { |f|
+      check_json = JSON.parse(f.read, :symbolize_names => true)[:checks]
       @check_name, @check =  check_json.keys.first, check_json.values.first
     }
 
     distributed_mutex.synchronize {
-      check["command"] = check.delete("actual_command")
-      check.merge!(
-        "executed" => Time.now.to_i,
-        "name" => check_name
-      )
-      check["output"] = `#{check["command"]}`
-      check["status"] = $?.exitstatus
-      send_event_to_local_sensu_client
+      @new_check = { :executed => Time.now.to_i }.merge(check)
+      new_check[:command] = new_check.delete(:actual_command)
+      new_check[:name] = new_check.delete(:actual_name)
+      new_check[:output] = `( #{new_check[:command]} ) 2>&1`
+      new_check[:status] = $?.success? ? 0 : 2
+      new_check[:issued] = Time.now.to_i
+      send_new_check_event_to_local_sensu_client
     }
 
-    ok
+    check[:foo] = 'bar'
+    check.delete :source
+    ok "noop"
   end
 
   def redis
-    @redis ||= TinyRedis::Client.new(host=configuration["redis_server"],
-                                     port=configuration["redis_port"])
+    @redis ||= TinyRedis::Client.new(host=configuration[:redis_server],
+                                     port=configuration[:redis_port])
   end
 
   def distributed_mutex
+    mutex_expiration = check[:interval] > 5 ? check[:interval] - 5 : 1
     @mutex = TinyRedis::Mutex.new(redis,
                                   "synchronized_check_mutex::::#{check_name}",
-                                  check["interval"] || 60,
+                                  mutex_expiration,
                                   $stdout)
   end
 
-  def send_event_to_local_sensu_client
-    check["issued"] = Time.now.to_i
-    sock = TCPSocket.new('127.0.0.1', configuration['sensu_client_port'])
-    sock.puts(check.to_json)
+  def send_new_check_event_to_local_sensu_client
+    sock = TCPSocket.new('127.0.0.1', configuration[:sensu_client_port])
+    sock.puts(new_check.to_json)
     sock.close
   end
 end
