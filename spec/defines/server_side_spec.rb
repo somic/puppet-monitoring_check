8b2e84c8980e23b367b28433933c0eefebe56d6b
Dmitriy Samovskiy <dmits@yelp.com>
2015-04-30 15:16:20 -0700

    renaming

--- spec/defines/server_side_spec.rb
+++ spec/defines/server_side_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-describe 'monitoring_check::synchronized' do
+describe 'monitoring_check::server_side' do
 
   let(:title) { 'example1' }
   let('::sensu::check_notify') { 'foo' }
 
   let(:hiera_data) {{
-    'monitoring_check::synchronized::install::redis_server' => 'redis1.example.com',
+    'monitoring_check::server_side::install::redis_server' => 'redis1.example.com',
     'monitoring_check::params::team_data' => { 'qux' => { } },
   }}
 
@@ -16,9 +16,9 @@ describe 'monitoring_check::synchronized' do
       :team => 'qux',
     }}
     it {
-      should contain_class('monitoring_check::synchronized::install')
-      should contain_monitoring_check('synchronized_placeholder_for_example1') \
-               .with_command(/check-synchronized.rb/) \
+      should contain_class('monitoring_check::server_side::install')
+      should contain_monitoring_check('server_side_placeholder_for_example1') \
+               .with_command(/check_server_side.rb/) \
                .with_sensu_custom({
                  :actual_command => 'foo',
                  :actual_name    => 'example1',
