e6f7db6afcab3c90b6792fe09ad68959f921248d
Dmitriy Samovskiy <dmits@yelp.com>
2015-05-01 15:45:57 -0700

    another tests fix

--- spec/defines/server_side_spec.rb
+++ spec/defines/server_side_spec.rb
@@ -9,10 +9,20 @@ describe 'monitoring_check::server_side' do
   }}
 
   context "by default" do
+    let(:pre_condition) { 'include sensu' }
     let(:params) {{
       :command => 'foo', :runbook => 'y/bar', :source => 'baz',
       :team => 'qux',
     }}
+    let(:facts) {{
+      :ipaddress => '127.0.0.1',
+      :osfamily => 'Debian',
+      :lsbdistid => 'Ubuntu',
+      :lsbdistcodename => 'Lucid',
+      :operatingsystem => 'Ubuntu',
+      :puppetversion => '3.6.2',
+    }}
+
     it {
       should contain_class('monitoring_check::server_side::install')
       should contain_monitoring_check('server_side_placeholder_for_example1') \
